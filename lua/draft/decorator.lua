53803985504dd92fe92a027bd79a055290ed7656
ubik <rreejjmmaann@gmail.com>
2026-03-14 02:14:32 +0100

    Refactor

--- lua/draft/decorator.lua
+++ lua/draft/decorator.lua
@@ -3,27 +3,13 @@ local M = {}
 
 local ns = vim.api.nvim_create_namespace("fiction")
 
-local start_i = 0
-local buf_i = 0
-local win_i = 0
-local line_i = 0
-local end_i = 0
--- end
-
 local draft_ns = vim.api.nvim_create_namespace("draft")
 
----@param buf string
----@param line_nr number
----@param length number
-local function make_indent(buf, line_nr, length)
-	local space = string.rep(" ", length)
-	vim.api.nvim_buf_set_extmark(buf, draft_ns, line_nr - 1, 0, {
-		virt_text = { { space, "NonText" } },
-		virt_text_pos = "inline",
-		hl_mode = "combine",
-	})
-end
-
+--[[
+	Pomysł na optymalizacje:
+	podczas edytowania, linia pobiera się niepotrzebnie za każdym nowym znakiem.
+	Zapisać pobraną liniie do cache i zmieniać ją tylko gdy zmieni się pozycja kursora
+]]
 ---@param line string
 ---@return boolean
 local function is_comment(line)
@@ -37,14 +23,14 @@ local function is_header(line)
 end
 
 ---@param hl_group string
----@param buf string
----@param line_nr number
+---@param buf_nr string
+---@param row_nr number
 ---@param start number | nil
 ---@param stop number | nil
-local function decore_as(hl_group, bufnr, row, start, stop)
+local function highlight_as(hl_group, buf_nr, row_nr, start, stop)
 	start = start or 0
 	stop = stop or -1
-	vim.api.nvim_buf_add_highlight(bufnr, draft_ns, hl_group, row, start, stop)
+	vim.api.nvim_buf_add_highlight(buf_nr, draft_ns, hl_group, row_nr, start, stop)
 end
 
 ---@return boolean
@@ -52,106 +38,97 @@ local function is_insert_mode()
 	return "i" == vim.api.nvim_get_mode().mode
 end
 
---[[
-function decorate_row(row)
+local cursor_pos = function(win_id)
+	return vim.api.nvim_win_get_cursor(win_id)[1] - 1
+end
 
+local is_cursor_in_row = function(win_id, row_nr)
+	return cursor_pos(win_id) == row_nr
+end
 
-end]]
+local get_line = function(buf_nr, row_nr)
+	return vim.api.nvim_buf_get_lines(buf_nr, row_nr, row_nr + 1, false)[1]
+end
 
-local draw_counter = {}
+local draw_counter = {} -- debug
+---@param buf string
+---@param line_nr number
+---@param length number
+local function make_indent(buf_nr, row_nr, length)
+	draw_counter[row_nr] = (draw_counter[row_nr] or 0) + 1 -- debug
+	local space = string.rep(" ", length)
+	vim.api.nvim_buf_set_extmark(buf_nr, draft_ns, row_nr, 0, {
+		virt_text = { { "->" .. draw_counter[row_nr] .. " ", "NonText" } },
+		--virt_text = { { space, "NonText" } },
+		virt_text_pos = "inline",
+		hl_mode = "combine",
+	})
+end
+
+local function make_quotes(buf_nr, row_nr, line)
+	for s, e in line:gmatch('()"[^"]+"()') do
+		highlight_as("Statement", buf_nr, row_nr, s - 1, e - 1)
+	end
+end
+
+local function make_dialogues(buf_nr, row_nr, line)
+	local line_len = #line
+	local pos = 1
+
+	while pos <= line_len do
+		local s, e = line:find("—", pos, true) -- pierwsz pałza
+		if not s then
+			break
+		end
+
+		local next_s, next_e = line:find("—", e + 1, true) -- dugia pałza
+		if next_s then
+			highlight_as("Statement", buf_nr, row_nr, s - 1, next_e)
+			pos = next_e + 1
+		else
+			-- jeśli nie ma drugiej pałzy
+			highlight_as("Statement", buf_nr, row_nr, s - 1, line_len)
+			break
+		end
+	end
+end
 
 vim.api.nvim_set_decoration_provider(ns, {
 
-	on_line = function(_, winid, bufnr, row)
-		local cursor_line = vim.api.nvim_win_get_cursor(winid)[1] - 1
+	on_line = function(_, win_id, buf_nr, row_nr)
+		if not is_cursor_in_row(win_id, row_nr) then
+			return false
+		end
 
-		if cursor_line == row then
-			draw_counter[row] = (draw_counter[row] or 0) + 1
-			vim.api.nvim_buf_clear_namespace(bufnr, -1, row, row + 1)
-			local line = vim.api.nvim_buf_get_lines(bufnr, row, row + 1, false)[1]
-			if is_comment(line) then
-				decore_as("NonText", bufnr, row, 0, #line)
-			else
-				-- decore_as("Normal", bufnr, row)
-				vim.api.nvim_buf_set_extmark(bufnr, ns, row, 0, {
-					virt_text = { { "->" .. draw_counter[row] .. " ", "NonText" } },
-					virt_text_pos = "inline",
-					hl_mode = "combine",
-				})
+		vim.api.nvim_buf_clear_namespace(buf_nr, -1, row_nr, row_nr + 1)
 
-				for s, e in line:gmatch('()"[^"]+"()') do
-					decore_as("Statement", bufnr, row, s - 1, e - 1)
-				end
-
-				local line_len = #line
-				local pos = 1
-
-				while pos <= line_len do
-					local s, e = line:find("—", pos, true) -- pierwsz pałza
-					if not s then
-						break
-					end
-
-					local next_s, next_e = line:find("—", e + 1, true) -- dugia pałza
-					if next_s then
-						decore_as("Statement", bufnr, row, s - 1, next_e)
-						pos = next_e + 1
-					else
-						-- jeśli nie ma drugiej pałzy
-						decore_as("Statement", bufnr, row, s - 1, line_len)
-						break
-					end
-				end
-				for s, e in line:gmatch('()"[^"]+"()') do
-					decore_as("Statement", bufnr, row, s - 1, e - 1)
-				end
-			end
+		local line = get_line(buf_nr, row_nr)
+		if is_comment(line) then
+			vim.api.nvim_buf_add_highlight(buf_nr, draft_ns, "NonText", row_nr, 0, #line)
+		else
+			make_indent(buf_nr, row_nr, 2)
+			make_quotes(buf_nr, row_nr, line)
+			make_dialogues(buf_nr, row_nr, line)
 		end
 	end,
 })
 
 ---@param buf string
 ---@param tab_length number
-function M.render(buf, tab_length)
-	vim.api.nvim_buf_clear_namespace(buf, draft_ns, 0, -1)
+function M.render(buf_nr, tab_length)
+	vim.api.nvim_buf_clear_namespace(buf_nr, -1, 0, -1)
 	-- vim.api.nvim_set_hl(0, "Quote", { italic = true })
 
-	local cursor_pos = vim.fn.line(".")
-	local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+	local lines = vim.api.nvim_buf_get_lines(buf_nr, 0, -1, false)
 
 	for nr, line in ipairs(lines) do
+		local row_nr = nr - 1
 		if is_comment(line) then
-			decore_as("NonText", buf, nr)
-		elseif is_header(line) then
-			if nr == cursor_pos and is_insert_mode() then
-				make_indent(buf, nr, tab_length)
-			else
-				make_indent(buf, nr, tab_length * 2)
-				decore_as("Title", buf, nr)
-			end
+			vim.api.nvim_buf_add_highlight(buf_nr, draft_ns, "NonText", row_nr, 0, #line)
 		else
-			make_indent(buf, nr, tab_length)
-			local line_len = #line
-			local pos = 1
-
-			while pos <= line_len do
-				local s, e = line:find("—", pos, true)
-				if not s then
-					break
-				end
-
-				local next_s, next_e = line:find("—", e + 1, true)
-				if next_s then
-					decore_as("Statement", buf, nr, s - 1, next_e)
-					pos = next_e + 1
-				else
-					decore_as("Statement", buf, nr, s - 1)
-					break
-				end
-			end
-			for s, e in line:gmatch('()"[^"]+"()') do
-				decore_as("Statement", buf, nr, s - 1, e - 1)
-			end
+			make_indent(buf_nr, row_nr, tab_length)
+			make_quotes(buf_nr, row_nr, line)
+			make_dialogues(buf_nr, row_nr, line)
 		end
 	end
 end
