39939940703181292542ec2712670da7a08f51aa
ubik <rreejjmmaann@gmail.com>
2026-03-14 15:31:08 +0100

    Bug

--- lua/draft/decorator.lua
+++ lua/draft/decorator.lua
@@ -1,9 +1,7 @@
 ---@module "decorator"
 local M = {}
 
-local ns = vim.api.nvim_create_namespace("fiction")
-
-local draft_ns = vim.api.nvim_create_namespace("draft")
+local ns = vim.api.nvim_create_namespace("draft")
 
 --[[
 	Pomysł na optymalizacje:
@@ -30,7 +28,7 @@ end
 local function highlight_as(hl_group, buf_nr, row_nr, start, stop)
 	start = start or 0
 	stop = stop or -1
-	vim.api.nvim_buf_add_highlight(buf_nr, draft_ns, hl_group, row_nr, start, stop)
+	vim.api.nvim_buf_add_highlight(buf_nr, ns, hl_group, row_nr, start, stop)
 end
 
 ---@return boolean
@@ -57,7 +55,7 @@ local draw_counter = {} -- debug
 local function make_indent(buf_nr, row_nr, length)
 	draw_counter[row_nr] = (draw_counter[row_nr] or 0) + 1 -- debug
 	local space = string.rep(" ", length)
-	vim.api.nvim_buf_set_extmark(buf_nr, draft_ns, row_nr, 0, {
+	vim.api.nvim_buf_set_extmark(buf_nr, ns, row_nr, 0, {
 		virt_text = { { "->" .. draw_counter[row_nr] .. " ", "NonText" } },
 		--virt_text = { { space, "NonText" } },
 		virt_text_pos = "inline",
@@ -92,44 +90,52 @@ local function make_dialogues(buf_nr, row_nr, line)
 		end
 	end
 end
+local function decore_row(buf_nr, row_nr)
+	vim.api.nvim_buf_clear_namespace(buf_nr, -1, row_nr, row_nr + 1)
 
+	local line = get_line(buf_nr, row_nr)
+	if is_comment(line) then
+		vim.api.nvim_buf_add_highlight(buf_nr, ns, "NonText", row_nr, 0, #line)
+	else
+		make_indent(buf_nr, row_nr, 2)
+		make_quotes(buf_nr, row_nr, line)
+		make_dialogues(buf_nr, row_nr, line)
+	end
+end
+
+local first_load = true
 vim.api.nvim_set_decoration_provider(ns, {
 
 	on_line = function(_, win_id, buf_nr, row_nr)
-		if not is_cursor_in_row(win_id, row_nr) then
+		if first_load or not is_cursor_in_row(win_id, row_nr) then
 			return false
 		end
-
-		vim.api.nvim_buf_clear_namespace(buf_nr, -1, row_nr, row_nr + 1)
-
-		local line = get_line(buf_nr, row_nr)
-		if is_comment(line) then
-			vim.api.nvim_buf_add_highlight(buf_nr, draft_ns, "NonText", row_nr, 0, #line)
-		else
-			make_indent(buf_nr, row_nr, 2)
-			make_quotes(buf_nr, row_nr, line)
-			make_dialogues(buf_nr, row_nr, line)
-		end
+		decore_row(buf_nr, row_nr)
+	end,
+	on_end = function()
+		first_load = false
 	end,
 })
 
 ---@param buf string
 ---@param tab_length number
 function M.render(buf_nr, tab_length)
-	vim.api.nvim_buf_clear_namespace(buf_nr, -1, 0, -1)
+	--vim.api.nvim_buf_clear_namespace(buf_nr, ns, 0, -1)
 	-- vim.api.nvim_set_hl(0, "Quote", { italic = true })
-
 	local lines = vim.api.nvim_buf_get_lines(buf_nr, 0, -1, false)
 
 	for nr, line in ipairs(lines) do
-		local row_nr = nr - 1
+		decore_row(buf_nr, nr - 1)
+		--[[		local row_nr = nr - 1
+		vim.api.nvim_buf_clear_namespace(buf_nr, -1, row_nr, row_nr + 1)
 		if is_comment(line) then
-			vim.api.nvim_buf_add_highlight(buf_nr, draft_ns, "NonText", row_nr, 0, #line)
+			vim.api.nvim_buf_add_highlight(buf_nr, ns, "NonText", row_nr, 0, #line)
 		else
-			make_indent(buf_nr, row_nr, tab_length)
+			make_indent(buf_nr, row_nr, 2)
 			make_quotes(buf_nr, row_nr, line)
 			make_dialogues(buf_nr, row_nr, line)
 		end
+		]]
 	end
 end
 
