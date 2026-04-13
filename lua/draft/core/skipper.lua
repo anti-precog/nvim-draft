7323b3689263feecf72707728c765080b901c6e6
ubik <rreejjmmaann@gmail.com>
2026-04-13 22:36:08 +0200

    Fix for meta skipper

--- lua/draft/core/skipper.lua
+++ lua/draft/core/skipper.lua
@@ -3,7 +3,7 @@ local utils = require("draft.utils")
 ---@param text string Line to check
 ---@return boolean is_meta Is the text meta
 local function is_meta(text)
-	return text:match("^[#].*")
+	return text:match("^[@#].*")
 end
 
 -- A submodule for skip lines
@@ -14,11 +14,17 @@ local prev_line = -1
 
 function M.try_skip()
 	local current_line = utils.get_cursor_line_nr()
+	local last_line = utils.get_last_line_nr()
 
 	if current_line > prev_line then
 		local text = vim.fn.getline(current_line)
-		if is_meta(text) then
+		while is_meta(text) do
 			vim.cmd("normal! j")
+			if current_line == last_line then
+				break
+			end
+			current_line = utils.get_cursor_line_nr()
+			text = vim.fn.getline(current_line)
 		end
 	end
 	prev_line = current_line
